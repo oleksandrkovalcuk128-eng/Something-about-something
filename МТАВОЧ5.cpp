5d1faafaa61c77b1d786f22910444ea3ed450be6
oleksandrkovalcuk128-eng <oleksandrkovalcuk128@gmail.com>
2026-05-13 21:52:05 +0200

    Add files via upload

---
+++ МТАВОЧ5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+	string name[4];
+
+	for (int i = 0; i < 4; i++) {
+		cout << i << ": ";
+		cin >> name[i];
+	}
+
+	for (int i = 0; i < 4; i++) {
+		cout << i << ". " << name[i] << endl;
+	}
+}
